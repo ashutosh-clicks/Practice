ed8784d31c7cd47fb79244876c0c8cc409098a49
Ashutosh <asusolan2016@gmail.com>
2025-10-29 19:50:48 +0530

    practcal 7 and 8

--- PythonMayaThapa/test.py
+++ PythonMayaThapa/test.py
@@ -1,4 +1,17 @@
-import PythonMayaThapa.testadd as testadd
-r = testadd.add(1,3)
-print(r)
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+f = open("PythonMayaThapa/text.txt",'r')
+print(f)
+# f.write("na kr yurr")
 
+# f.read(5)
+
+a = np.arange(10)
+b = np.arange(0,100,10)
+
+
+
+# f.close()
+os.cpu_count
